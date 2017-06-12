7f5f33ebbec41f85da81658cea3d4c951ff7ce55
Jose Medrano <jose.medrano@microsoft.com>
2017-06-12 12:50:49 +0200

    Adds new Hermes MQTT client/server example

---
+++ src/Hermes.Mqtt/IoTMqttTest.Server/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Net.Mqtt;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace IoTMqttTest.Server
+{
+	class Program
+	{
+		const int loopDelayTime = 250;
+		const string topic = "test/chat/message";
+		const string exitMessage = "exit";
+
+		static void Main (string[] args)
+		{
+			bool isFinishing = false;
+			var deviceIpAddress = GetLocalIPAddress();
+			var config = new MqttConfiguration { Port = 55555 };
+			var server = MqttServer.Create (config);
+
+			server.Start ();
+
+			var client = server.CreateClientAsync ().Result;
+			var clientId = client.Id;
+			var received = "";
+
+			client.SubscribeAsync (topic, MqttQualityOfService.AtLeastOnce).Wait();
+			client.MessageStream.Subscribe (message => {
+				if (isFinishing)
+					return;
+				var data = Encoding.UTF8.GetString (message.Payload).Split (new string[] { ":" }, StringSplitOptions.None);
+				Console.WriteLine ($"Message Received from {data[0]}: {data[1]}");
+
+				received = data[1];
+
+				PublishAsync (client, clientId, exitMessage);
+
+				if (received == exitMessage)
+					isFinishing = true;
+			});
+
+			Console.WriteLine ($"Server {deviceIpAddress}:{config.Port} with topic '{topic}' is up.");
+			Console.WriteLine ($"Awaiting messages...");
+
+			while (received != exitMessage) {
+				Thread.Sleep (loopDelayTime);
+			}
+			Console.WriteLine("Shutting down... Received exit command.");
+		}
+
+		static void PublishAsync (IMqttConnectedClient client, string clientId, string message)
+		{
+			Console.WriteLine ($"Sending message to {clientId}: {message}");
+			client.PublishAsync (new MqttApplicationMessage (topic, Encoding.UTF8.GetBytes ($"{clientId}:{message}")), MqttQualityOfService.AtLeastOnce).Wait();
+		}
+
+		public static string GetLocalIPAddress()
+		{
+			var host = Dns.GetHostEntry (Dns.GetHostName());
+			foreach (var ip in host.AddressList) {
+				if (ip.AddressFamily == AddressFamily.InterNetwork)
+					return ip.ToString();
+			}
+			throw new Exception("Local IP Address Not Found!");
+		}
+	}
+}
---
+++ src/Hermes.Mqtt/MqttTest.Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Mqtt;
+using System.Text;
+using System.Threading;
+
+namespace MqttTest.Client
+{
+	class Program
+	{
+		const int loopDelayTime = 250;
+		const string topic = "test/chat/message";
+		const string exitMessage = "exit";
+
+		static void Main (string[] args)
+		{
+			bool isFinishing = false;
+			var config = new MqttConfiguration { Port = 55555 };
+			var client = MqttClient.CreateAsync ("10.67.1.69", config).Result;
+			var clientId = "XamarinClient";
+			var received = "";
+
+			client.ConnectAsync (new MqttClientCredentials (clientId)).Wait ();
+			client.SubscribeAsync (topic, MqttQualityOfService.AtLeastOnce).Wait ();
+			client.MessageStream.Subscribe (message => {
+				if (isFinishing)
+					return;
+				
+				var data = Encoding.UTF8.GetString (message.Payload).Split (new string[] { ":" }, StringSplitOptions.None);
+				Console.WriteLine ($"Message Received from {data[0]}:{data[1]}");
+				PublishAsync (client, clientId, exitMessage);
+
+				//Send exit to server
+				received = data[1];
+
+				if (received == exitMessage)
+					isFinishing = true;
+			});
+
+			Console.WriteLine ($"Client connected successfully to {client.Id}:{config.Port}");
+			Console.WriteLine ($"Awaiting messages...");
+
+			PublishAsync (client, clientId, "Connected.");
+
+			while (received != exitMessage) {
+				Thread.Sleep (loopDelayTime);
+			}
+			Console.WriteLine ("Shutting down... Received exit command.");
+		}
+
+		static void PublishAsync (IMqttClient client, string clientId, string message)
+		{
+			Console.WriteLine ($"Sending message: {message}");
+			client.PublishAsync (new MqttApplicationMessage (topic, Encoding.UTF8.GetBytes ($"{clientId}:{message}")), MqttQualityOfService.AtLeastOnce).Wait();
+		}
+	}
+}
